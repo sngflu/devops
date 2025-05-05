233589dd7f2a02cf75525d3c73d2aa2eb6233734
sngflu <sngflu@yandex.ru>
2025-05-06 01:05:04 +0300

    VM version

--- project/main.tf
+++ project/main.tf
@@ -1,37 +1,66 @@
-# Terraform configuration to maintain VM metadata
-# This file documents the VM configuration using Terraform syntax
-# The VM itself is created using VBoxManage commands
-
 terraform {
   required_providers {
-    null = {
-      source = "hashicorp/null"
-      version = "~> 3.0"
+    yandex = {
+      source = "yandex-cloud/yandex"
     }
   }
+  required_version = ">= 0.13"
 }
 
-resource "null_resource" "documentation" {
-  triggers = {
-    vm_name = "lab2-docker-vm"
-    cpus = "2"
-    memory = "2048"
-    os_type = "Ubuntu_64"
-    ssh_port = "2222"
-    hostonly_ip = "192.168.56.10"
+provider "yandex" {
+  service_account_key_file = "${file("./authorized_key.json")}"
+  zone = "ru-central1-a"
+}
+
+resource "yandex_compute_disk" "boot-disk-1" {
+  name     = "boot-disk-1"
+  type     = "network-hdd"
+  zone     = "ru-central1-a"
+  size     = "30"
+  image_id = "fd86601pa1f50ta9dffg"
+  folder_id = "b1g3ljevgl8gifeo83pj"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
+  folder_id = "b1g3ljevgl8gifeo83pj"
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.boot-disk-1.id
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("./meta.txt")}"
   }
 }
 
-output "connection_info" {
-  value = "VM can be accessed via SSH at: ssh -p 2222 ubuntu@127.0.0.1"
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+  folder_id = "b1g3ljevgl8gifeo83pj"
 }
 
-output "services" {
-  value = {
-    frontend = "http://127.0.0.1:5173"
-    backend = "http://127.0.0.1:5174"
-    pgadmin = "http://127.0.0.1:5050"
-    minio_console = "http://127.0.0.1:9001"
-    minio_api = "http://127.0.0.1:9000"
-  }
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+  folder_id = "b1g3ljevgl8gifeo83pj"
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
 }
